40fc4df327bcbd7bb0cd1f4c3b5d6921e2f35beb
Evgeni Pandurski <epandurski@gmail.com>
2018-08-11 18:17:45 +0300

    Make the entrypoint run migrations upgrade

--- services/users/docker-flask/entrypoint.sh
+++ services/users/docker-flask/entrypoint.sh
@@ -2,28 +2,77 @@
 #
 # This file is used by "Dockerfile-flask".
 
+LOGGING_CONF='/logging.conf'
+GUNICORN_CONF='/gunicorn.conf'
+
+py_log_error() {
+    python - <<EOF
+import logging
+import logging.config
+logging.config.fileConfig('$LOGGING_CONF')
+logging.error(open('$1').read())
+EOF
+}
+
+flask_db_upgrade() {
+    N_ERRORS_TO_IGNORE=6
+    RETRY_AFTER=1
+    ERROR_FILE='/flask-db-upgrade.error'
+    echo -n "Running flask db upgrade ... "
+    while true; do
+        set +e
+        flask db upgrade 2>$ERROR_FILE
+        ERROR_CODE=$?
+        set -e
+        case $ERROR_CODE in
+            0)
+                echo "ok"
+                return 0
+                ;;
+            1)
+                TIME_LIMIT=$((1 << N_ERRORS_TO_IGNORE))
+                [[ $RETRY_AFTER -eq $TIME_LIMIT ]] && echo "retrying"
+                [[ $RETRY_AFTER -ge $TIME_LIMIT ]] && py_log_error "$ERROR_FILE"
+                sleep $RETRY_AFTER
+                RETRY_AFTER=$((2 * RETRY_AFTER))
+                ;;
+            2)
+                echo "not installed"
+                return 0
+                ;;
+            *)
+                echo "error"
+                return $ERROR_CODE
+                ;;
+        esac
+    done
+}
+
 export PYTHONDONTWRITEBYTECODE=1
 
 case $1 in
     develop)
         shift;
         export FLASK_ENV=development
-        exec flask run --host=0.0.0.0 --port 80 "$@"
+        flask run --host=0.0.0.0 --port 80 "$@"
         ;;
     debug)
         shift;
         export FLASK_ENV=development
-        exec python -m pudb wsgi.py "$@"
+        python -m pudb wsgi.py "$@"
         ;;
     db)
         shift;
-        exec flask db "$@"
+        flask db "$@"
         ;;
     '')
         export -n PYTHONDONTWRITEBYTECODE
-        exec gunicorn --config /gunicorn.conf --log-config /logging.conf -b :80 wsgi:app
+        flask_db_upgrade
+        gunicorn --config "$GUNICORN_CONF" \
+                 --log-config "$LOGGING_CONF" \
+                 -b :80 wsgi:app
         ;;
     *)
-        exec "$@"
+        "$@"
         ;;
 esac
