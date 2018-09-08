ea000047ae0acd29c9f12c7a55f12b84fe302c76
Evgeni Pandurski <epandurski@gmail.com>
2018-09-08 15:01:51 +0300

    Add pg-create-users and hydra-migrate containters

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,27 @@
-CREATE FUNCTION create_user_with_own_schema(username text, password text DEFAULT NULL) RETURNS void
+CREATE OR REPLACE FUNCTION create_user_with_own_schema(username text, password text DEFAULT NULL) RETURNS void
 AS $$
 BEGIN
   EXECUTE format('CREATE USER %I WITH PASSWORD %L', username, COALESCE(password, username));
   EXECUTE format('CREATE SCHEMA AUTHORIZATION %I', username);
+  RAISE NOTICE 'created role "%"', username;
+EXCEPTION
+  WHEN duplicate_object THEN
+    RAISE NOTICE 'role "%" already exists ', username;
 END;
 $$ LANGUAGE plpgsql;
 
 
-SELECT create_user_with_own_schema('users_service');
+CREATE OR REPLACE FUNCTION create_users_with_own_schemas() RETURNS text
+AS $$
+BEGIN
+  --------------------------
+  -- Add your users here! --
+  --------------------------
+  PERFORM create_user_with_own_schema('users_service');
+  PERFORM create_user_with_own_schema('hydra_service');
+  RETURN 'ok';
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT create_users_with_own_schemas();
