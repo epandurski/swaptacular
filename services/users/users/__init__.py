06c6e5c225a8334ba5bc2eb14150efc0a8e86c81
Evgeni Pandurski <epandurski@gmail.com>
2018-11-10 20:52:25 +0200

    Use create_app()

--- services/users/users/__init__.py
+++ services/users/users/__init__.py
@@ -6,10 +6,13 @@ from . import models, emails, redis, routes
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-app = Flask(__name__)
-app.config.from_object(Configuration)
 
-models.init_app(app)
-emails.init_app(app)
-redis.init_app(app)
-routes.init_app(app)
+
+def create_app(config_object=None):
+    app = Flask(__name__)
+    app.config.from_object(config_object or Configuration)
+    models.init_app(app)
+    emails.init_app(app)
+    redis.init_app(app)
+    routes.init_app(app)
+    return app
