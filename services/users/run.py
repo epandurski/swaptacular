d3cfe59fdd98892f0349148d48ef07ed508c16ef
Evgeni Pandurski <epandurski@gmail.com>
2018-11-13 09:07:12 +0200

    Do not pass threaded=False to app.run(), also -- do not import pudb

--- services/users/run.py
+++ services/users/run.py
@@ -9,10 +9,4 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    try:
-        from pudb import set_trace
-    except ImportError:
-        pass
-    else:
-        set_trace(paused=False)
-    app.run(host='0.0.0.0', port=app.config.get('PORT', 8000), debug=True, use_reloader=False, threaded=False)
+    app.run(host='0.0.0.0', port=app.config.get('PORT', 8000), debug=True, use_reloader=False)
