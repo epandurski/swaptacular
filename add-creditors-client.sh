97d51de45a5ebd9bd7add01b5441bf07bf78dcd6
Evgeni Pandurski <epandurski@gmail.com>
2021-02-24 15:01:38 +0200

    Move client's json files

--- add-creditors-client.sh
+++ add-creditors-client.sh
@@ -2,5 +2,12 @@
 set -e
 . ./export_vars.sh
 
+if [ -z "$1" ]; then
+    echo "Usage: add-creditors-client.sh FILE"
+    echo
+    echo "Creates a new OAuth2 creditors client from a JSON description file."
+    exit 2
+fi
+
 filepath="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 docker-compose run --volume="$filepath:/client.json" creditors-login hydra clients import /client.json --fake-tls-termination
--- add-debtors-client.sh
+++ add-debtors-client.sh
@@ -2,5 +2,12 @@
 set -e
 . ./export_vars.sh
 
+if [ -z "$1" ]; then
+    echo "Usage: add-debtors-client.sh FILE"
+    echo
+    echo "Creates a new OAuth2 debtors client from a JSON description file."
+    exit 2
+fi
+
 filepath="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 docker-compose run --volume="$filepath:/client.json" debtors-login hydra clients import /client.json --fake-tls-termination
